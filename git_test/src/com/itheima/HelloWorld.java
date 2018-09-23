a11a6ce46a6353050c3b25d5245c693f5be9627f
xiaohuiwu <1079374992@qq.com>
2018-09-23 10:29:19 +0800

    tijiao

---
+++ git_test/src/com/itheima/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("bbbbbbbbbbbb");
+        System.out.println("bbbbbbbbbbbb");
+        System.out.println("bbbbbbbbbbbb");
+        System.out.println("bbbbbbbbbbbb");
+        System.out.println("bbbbbbbbbbbb");
+    }
+    public void say(){
+        System.out.println("hello");
+    }
+}
